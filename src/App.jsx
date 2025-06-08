d3d428f0a54ac0d180d8f845d963f8892776b984
JuanMaGaviria <juan.gaviria@lambdananalytics.co>
2025-06-07 20:48:46 -0500

    fix(proyecto): correccion de rutas del sistema movimientos 8

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ import Sidebar from './modules/core/Sidebar/components/Sidebar.jsx';
 import ProtectedRoute from './modules/core/ProtectedRoute/components/ProtectedRoute.jsx';
 
 // importaciones de modulos
-import Inicio from './modules/Inicio/components/Inicio.jsx';
-import Usuarios from './modules/Usuarios/components/Usuarios.jsx';
-import Articulos from './modules/Articulos/components/Articulos.jsx';
-import Movimientos from './modules/Movimientos/components/Movimientos.jsx';
+import Inicio from './modules/inicio/components/Inicio.jsx';
+import Usuarios from './modules/usuarios/components/Usuarios.jsx';
+import Articulos from './modules/articulos/components/Articulos.jsx';
+import Movimientos from './modules/movimientos/components/Movimientos.jsx';
 
 const App = () => {
     return (
